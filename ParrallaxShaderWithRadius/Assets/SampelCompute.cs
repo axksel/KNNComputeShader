ad3f2cbe1665799c605fa870b7216859c1564e58
axksel <jonas.aksel@gmail.com>
2019-07-09 15:14:23 +0200

    ready for knn

--- ParrallaxShaderWithRadius/Assets/SampelCompute.cs
+++ ParrallaxShaderWithRadius/Assets/SampelCompute.cs
@@ -7,12 +7,20 @@ public class SampelCompute : MonoBehaviour
 
     public ComputeShader compute;
     public RenderTexture result;
+    public int amountOfObjects = 50;
+    public GameObject objectPrefab;
+    public GameObject playerPrefab;
     int kernel;
-    public float alpha=1.0f;
-    public int[] randomData = new int[] { 15, 1645, 135, 567 };
+
+
+
+    public Vector3[] randomPoints;
     // Start is called before the first frame update
     void Start()
     {
+        randomPoints = new Vector3[amountOfObjects];
+
+        InstantiateObjects();
         kernel = compute.FindKernel("CSMain");
 
         result = new RenderTexture(512, 512, 24);
@@ -20,19 +28,21 @@ public class SampelCompute : MonoBehaviour
         result.Create();
 
 
-        compute.SetFloat("a", alpha);
-        compute.SetInt("amountOfObjects", randomData.Length);
+       
+        compute.SetInt("amountOfObjects", randomPoints.Length);
+        compute.SetVector("playerObject", playerPrefab.transform.position);
 
-        var buffer = new ComputeBuffer(randomData.Length, sizeof(int));
-        buffer.SetData(randomData);
-        compute.SetBuffer(kernel,"RD", buffer);
+        var buffer = new ComputeBuffer(randomPoints.Length, sizeof(float)*3);
+        buffer.SetData(randomPoints);
+        compute.SetBuffer(kernel,"RP", buffer);
 
         compute.SetTexture(kernel, "Result", result);
-        
+
         compute.Dispatch(kernel, 512 / 8, 512 / 8, 1);
 
-        buffer.GetData(randomData);
+        buffer.GetData(randomPoints);
         buffer.Release();
+
 
     }
 
@@ -40,9 +50,22 @@ public class SampelCompute : MonoBehaviour
     void Update()
     {
         
-        compute.SetFloat("a", alpha);
-        compute.Dispatch(kernel, 512 / 8, 512 / 8, 1);
+        //compute.SetFloat("a", alpha);
+        //compute.Dispatch(kernel, 512 / 8, 512 / 8, 1);
         
+
+    }
+
+    public void InstantiateObjects()
+
+    {
+
+        for (int i = 0; i < amountOfObjects; i++)
+        {
+            GameObject tmp = Instantiate(objectPrefab);
+            tmp.transform.position = new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+            randomPoints[i] = tmp.transform.position;
+        }
 
     }
 }
